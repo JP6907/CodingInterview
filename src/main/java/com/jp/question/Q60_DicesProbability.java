617861e9b3edc1c3f8e6234116bbb273bf9b817c
JP6907 <jp6907@163.com>
2020-01-08 21:43:37 +0800

    60 61 62

---
+++ src/main/java/com/jp/question/Q60_DicesProbability.java
@@ -0,0 +1,52 @@
+package com.jp.question;
+
+// 面试题60：n个骰子的点数
+// 题目：把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s
+// 的所有可能的值出现的概率。
+
+import java.util.ArrayList;
+import java.util.List;
+
+// n个骰子，和最小为n，最大为6×n，一共6n-n+1种和
+// 和为 s 保存在 s-n 中
+public class Q60_DicesProbability {
+
+    /**
+     *
+     * @param number 骰子数量
+     */
+    static void PrintProbaility(int number){
+        int maxSum = number*6;
+        List<Integer> list = new ArrayList<Integer>(6*number-number+1);
+        for(int i=number;i<=maxSum;i++)
+            list.add(0);
+
+        Probaility(number,0,0,list);
+
+        double total = Math.pow(6.0,number);
+        for(int i=number;i<=maxSum;i++){
+            double ratio = (double)list.get(i-number)/total;
+            System.out.format("%d:%e\n",i,ratio);
+        }
+    }
+    /**
+     *
+     * @param origin 骰子数量
+     * @param current 当前计算到第几个骰子
+     * @param sum 当前累加和
+     * @param nProbailities 累加结果出现的次数
+     */
+    static void Probaility(int origin,int current,int sum,List<Integer> nProbailities){
+            if(current==origin){
+                nProbailities.set(sum-origin,nProbailities.get(sum-origin)+1);
+            }else{
+                for(int i=1;i<=6;i++){
+                    Probaility(origin,current+1,sum+i,nProbailities);
+                }
+            }
+    }
+
+    public static void main(String[] args){
+        PrintProbaility(6);
+    }
+}
---
+++ src/main/java/com/jp/question/Q61_ContinousCards.java
@@ -0,0 +1,48 @@
+package com.jp.question;
+
+// 面试题61：扑克牌的顺子
+// 题目：从扑克牌中随机抽5张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+// 2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王可以看成任意数字。
+
+import java.util.Arrays;
+
+public class Q61_ContinousCards {
+
+    static boolean IsContinuous(int[] number){
+        assert number.length > 0;
+        Arrays.sort(number);
+        int zeroCount = 0;
+        int gapCount = 0;
+        //统计number中0的数量
+        for(int i=0;i<number.length&&number[i]==0;i++)
+            zeroCount++;
+        //统计number中间隔的数量
+        int small = zeroCount;
+        int big = small+1;
+        while(big<number.length){
+            //如果相临两张牌一样，则false
+            if(number[big]==number[small])
+                return false;
+            //间隔为相邻两张牌的数字差值大小
+            gapCount += number[big]-number[small]-1;
+            small = big++;
+        }
+        //0的数量是否大于间隔数量，是则可以为顺子
+        return zeroCount>=gapCount;
+    }
+
+    public static void main(String[] args){
+        assert IsContinuous(new int[]{ 1, 3, 2, 5, 4 }) == true;
+        System.out.println(IsContinuous(new int[]{ 1, 3, 2, 5, 4 }));
+        assert IsContinuous(new int[]{ 1, 3, 2, 6, 4 }) == false;
+        System.out.println(IsContinuous(new int[]{ 1, 3, 2, 6, 4 }));
+        assert IsContinuous(new int[]{ 0, 3, 2, 6, 4 }) == true;
+        System.out.println(IsContinuous(new int[]{ 0, 3, 2, 6, 4 }));
+        assert IsContinuous(new int[]{ 0, 3, 1, 6, 4 }) == false;
+        System.out.println(IsContinuous(new int[]{ 0, 3, 1, 6, 4 }));
+        assert IsContinuous(new int[]{ 1, 3, 0, 5, 0 }) == true;
+        System.out.println(IsContinuous(new int[]{ 1, 3, 0, 5, 0 }));
+        assert IsContinuous(new int[]{ 1, 3, 0, 7, 0 }) == false;
+        System.out.println(IsContinuous(new int[]{ 1, 3, 0, 7, 0 }));
+    }
+}
